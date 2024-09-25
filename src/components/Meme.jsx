90e3817fe8fd8fb5dacb92628c35c0457d93c681
Anastasiia <vasilisandreeva00@gmail.com>
2024-09-25 17:43:41 +0300

    add state to meme generator

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import memesData from "../memesData.js";
 
 export default function Meme() {
+  const [memeImage, setMemeImage] = useState('');
+
+  function getRandomIndex(max) {
+    return Math.floor(Math.random() * max) + 1;
+  }
+
+  function getMemeImage(ev) {
+    ev.preventDefault();
+
+    const memesArray = memesData?.data?.memes;
+
+    if (memesArray && memesArray.length > 0) {
+      const randomIndex = getRandomIndex(memesArray.length);
+      setMemeImage(memesArray[randomIndex].url);
+    }
+  }
+
   return (
     <main>
       <form action="" className="form">
@@ -12,8 +30,10 @@ export default function Meme() {
         <input type="text" className="form--input"/>
       </label>
 
-      <button className="form--btn">Get a new meme image  🖼</button>
+      <button onClick={getMemeImage} className="form--btn">Get a new meme image  🖼</button>
     </form>
+
+      {memeImage && (<img className="image" src={memeImage} alt="random generated image"/>)}
     </main>
   )
 }
