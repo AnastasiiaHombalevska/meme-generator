47cb26f937826dd357c324ae94bf4fa4895b6bcb
Anastasiia <vasilisandreeva00@gmail.com>
2024-10-02 18:18:10 +0300

    add files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/meme-generator/',
+  base: 'https://anastasiiahombalevska.github.io/meme-generator/',
   plugins: [react()],
 })
